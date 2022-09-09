282977bf8af8219286481600904ced129a9235ce
ben.yuan@servian.com <ben.yuan@servian.com>
2022-09-09 07:43:14 +0000

    replace customer_order with windows functions

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -34,43 +34,53 @@ paid_orders as (
     left join completed_payments on orders.order_id = completed_payments.order_id
     left join customers on orders.customer_id = customers.customer_id 
 ),
-customer_orders as (
-    select  customers.customer_id,
-            min(orders.order_placed_at) as first_order_date,
-            max(orders.order_placed_at) as most_recent_order_date,
-            count(orders.order_id) as number_of_orders
-    from customers  
-    left join orders on orders.customer_id = customers.customer_id 
-    group by 1
-), 
+--customer_orders as (
+--    select  customers.customer_id,
+--            min(orders.order_placed_at) as first_order_date,
+--            max(orders.order_placed_at) as most_recent_order_date,
+--            count(orders.order_id) as number_of_orders
+--    from customers  
+--    left join orders on orders.customer_id = customers.customer_id 
+--   group by 1
+--), 
 --final CTEs
 final as (
     select
-        paid_orders.order_id,
-        paid_orders.customer_id,
-        paid_orders.order_placed_at,
-        paid_orders.order_status,
-        paid_orders.total_amount_paid,
-        paid_orders.payment_finalized_date,
-        paid_orders.customer_first_name,
-        paid_orders.customer_last_name,
+        order_id,
+        customer_id,
+        order_placed_at,
+        order_status,
+        total_amount_paid,
+        payment_finalized_date,
+        customer_first_name,
+        customer_last_name,
 
-        row_number() over (order BY paid_orders.order_id) as transaction_seq,
+        row_number() over (order BY order_id) as transaction_seq,
 
-        row_number() over (partition by paid_orders.customer_id order BY paid_orders.order_id) as customer_sales_seq,
+        row_number() over (partition by customer_id order BY order_id) as customer_sales_seq,
 
-        case when customer_orders.first_order_date = paid_orders.order_placed_at
-        then 'new'
+        case  
+        when (
+            rank() over (
+            partition by customer_id
+            order by order_placed_at, order_id
+            ) = 1
+        ) then 'new'
         else 'return' end as nvsr,
 
         --x.clv_bad as customer_lifetime_value,
-        sum(paid_orders.total_amount_paid) over (
-            partition by paid_orders.customer_id
-            order by paid_orders.order_placed_at
+        sum(total_amount_paid) over (
+            partition by customer_id
+            order by order_placed_at
         ) as customer_lifetime_value, 
-        customer_orders.first_order_date as fdos
+
+        --first day of sale
+        first_value(order_placed_at) over (
+        partition by customer_id
+        order by order_placed_at
+        ) as fdos
     from paid_orders 
-    left join customer_orders on paid_orders.customer_id=customer_orders.customer_id
+    --left join customer_orders on paid_orders.customer_id=customer_orders.customer_id
     --left outer join 
     --(
     --    select
@@ -81,7 +91,7 @@ final as (
     --   group by 1
     --   order by p.order_id
     --)  x on x.order_id = p.order_id
-    order by paid_orders.order_id
+    order by order_id
 )
 
 select * from final
